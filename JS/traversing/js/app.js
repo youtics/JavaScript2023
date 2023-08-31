c3be9557c9fdaa08dcf2dfdf45ce5f645a3d9b8a
youtics <88842584+youtics@users.noreply.github.com>
2023-08-31 11:06:23 -0300

    Traversing de DOM

---
+++ JS/traversing/js/app.js
@@ -0,0 +1,11 @@
+
+const menu = document.querySelector('nav.menu')
+console.log(menu.children)
+console.log(menu.childNodes)
+console.log(menu.children[1].innerHTML)
+
+const nuestrosCursos = document.querySelector('.nuestros-cursos')
+console.log(nuestrosCursos.children[1].children[1])
+
+
+
